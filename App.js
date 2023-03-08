cf8762bf2d6515813914bdaf03b3c01eddb2ea9c
krishnaa-14 <68679770+krishnaa-14@users.noreply.github.com>
2023-03-08 21:40:54 +0530

    Chapter2 - Igniting App

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+const heading = React.createElement("h1", {
+
+}, "Namaste React");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
+console.log("Working");
+
